5b6ed66d8bfdde8c2289b0ab0e696504c3380465
Saul Calleja <scalleja12345@gmail.com>
2022-02-01 15:56:00 -0500

    Figured Out how to change colors to users prefereance

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const canvas = document.getElementById("draw");
 const clear = document.getElementById("clear");
+const eraser = document.getElementById("eraser");
 const context = canvas.getContext("2d");
-
+const redButton = document.getElementById("red");
+const blackButton = document.getElementById("black");
 resize();
 
+//Resize teh width of canvas
 function resize() {
   context.canvas.width = window.innerWidth;
   context.canvas.height = window.innerHeight;
@@ -16,14 +19,51 @@ function setPosition(e) {
   pos.y = e.clientY;
 }
 
+///
+///Mouse Hover Effect
+document.addEventListener("DOMContentLoaded", () => {
+  mouseCircle = document.getElementById("mouse-circle");
+
+  document.onmousemove = (e) => {
+    setPosition(e);
+  };
+
+  let delay = 2,
+    revisedPosX = 0,
+    revisedPosY = 0;
+
+  function delayMouseFollow() {
+    requestAnimationFrame(delayMouseFollow);
+
+    revisedPosX += (pos.x - revisedPosX) / delay;
+    revisedPosY += (pos.y - revisedPosY) / delay;
+
+    mouseCircle.style.top = revisedPosY + 110 + "px";
+    mouseCircle.style.left = revisedPosX + "px";
+  }
+  delayMouseFollow();
+});
+///
+
+//Drawing
 function draw(e) {
   if (e.buttons !== 1) return;
 
-  const color = document.getElementById("hex").value;
+  let color = document.getElementById("hex").value;
+
+  redButton.onclick = function () {
+    color = "#880015";
+    document.getElementById("hex").value = color;
+  };
+
+  blackButton.onclick = function () {
+    color = "#000000";
+    document.getElementById("hex").value = color;
+  };
 
   context.beginPath();
 
-  context.lineWidth = 2;
+  context.lineWidth = 5;
   context.lineCap = "round";
   context.strokeStyle = color;
 
@@ -34,12 +74,35 @@ function draw(e) {
   context.stroke();
 }
 
+//Clear The Canvas
 clear.addEventListener("click", clearCanvas);
 
 function clearCanvas() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+//Eraser
+
+eraser.addEventListener("click", Erase);
+
+function Erase() {
+  context.globalCompositeOperation = "destination-out";
+}
+
+//change Colors
+
+// redButton.addEventListener("click", changeColorToRed);
+// function changeColorToRed() {
+//   let color = document.getElementById("red").value;
+//   context.strokeStyle = color;
+//   console.log(color);
+
+//   context.strokeStyle = color;
+//   console.log(color);
+//   return color;
+// }
+
+//Event listeners
 window.addEventListener("resize", resize);
 
 document.addEventListener("mousemove", draw);
